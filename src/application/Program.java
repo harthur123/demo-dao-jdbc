3f3b8f01d851bb55f80cce235e71ce92ef186cbb
harthur <harthurhenrique214@gmail.com>
2025-05-08 15:58:46 -0300

    DepartmentDao and SellerDao interfaces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,7 @@
 package application;
 
-import models.entities.Department;
-import models.entities.Seller;
+import model.entities.Department;
+import model.entities.Seller;
 
 import java.util.Date;
 
@@ -12,7 +12,7 @@ public class Program {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Bobby", "bobby@gmail.com", new Date(), 3000.00, obj);
 
-        
+
         System.out.println(seller);
 
     }
